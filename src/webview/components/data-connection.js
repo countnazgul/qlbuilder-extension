68a5501430cf733d8ce296cc2c44cd4fe8a654a1
Stefan Stoichev <stefan.stoichev@gmail.com>
2019-11-04 10:04:19 +0000

    data connection - keep style

--- src/webview/components/data-connection.js
+++ src/webview/components/data-connection.js
@@ -12,30 +12,47 @@ Vue.component('data-connection', {
         }
     },
     computed: {
-        currentDataConnection() {
-            return store.getters.currentDataConnection
-        },
+        currentStyle: function() {
+            let self = this
+            if (self.currentDataConnection.label == self.dc.label) {
+                return "font-weight: bold; background-color: grey"
+                // self.bold = {
+                //     'font-weight': 'bold'
+                // }
+
+                // self.backgroundColor = {
+                //     'background-color': 'grey'
+                // }
+            } else {
+                return "font-weight: normal"
+                // self.bold = { 'font-weight': 'normal' }
+                // self.backgroundColor = {}
+            }
+        }        
+        // currentDataConnection() {
+        //     return store.getters.currentDataConnection
+        // },
     },
     watch: {
-        currentDataConnection(newConnection, oldCount) {
-            let self = this
-            if (newConnection.label == self.dc.label) {
-                self.bold = {
-                    'font-weight': 'bold'
-                }
+        // currentDataConnection(newConnection, oldCount) {
+        //     let self = this
+        //     if (newConnection.label == self.dc.label) {
+        //         self.bold = {
+        //             'font-weight': 'bold'
+        //         }
 
-                self.backgroundColor = {
-                    'background-color': 'grey'
-                }
-            } else {
-                self.bold = { 'font-weight': 'normal' }
-                self.backgroundColor = {}
-            }
-        }
+        //         self.backgroundColor = {
+        //             'background-color': 'grey'
+        //         }
+        //     } else {
+        //         self.bold = { 'font-weight': 'normal' }
+        //         self.backgroundColor = {}
+        //     }
+        // }
     },
     template: `
-    <li class="lui-list__item" :style="backgroundColor">
-        <span class="lui-list__aside lui-icon  lui-icon--folder" aria-hidden="true" :style="bold"></span>
+    <li class="lui-list__item" :style="currentStyle">
+        <span class="lui-list__aside lui-icon  lui-icon--folder" aria-hidden="true" :style=""></span>
         <span @click="getFiles" class="lui-list__text link" :style="bold">{{dc.label}}</span>
     </li>
     `,
